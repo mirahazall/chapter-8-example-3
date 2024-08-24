5df03b9c8b680bdf629adee54dfceb3f40dd1b88
mirahazall <s223530601@deakin.edu.au>
2024-08-24 22:51:38 +1000

    docker build image shell script changes to allow building multi-platform Docker Images

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -15,8 +15,4 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-# Enable Docker Buildx for multi-platform builds
-docker buildx create --use
-
-# Build and push the Docker image for both ARM and x86_64 platforms
-docker buildx build --platform linux/amd64,linux/arm64 -t $CONTAINER_REGISTRY/video-streaming:$VERSION --file ./Dockerfile-prod .
+docker build -t $CONTAINER_REGISTRY/video-streaming:$VERSION --file ./Dockerfile-prod .
