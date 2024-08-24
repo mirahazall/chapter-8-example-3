cc33fbd098e36d1a0104392fbaa37b55335ca96d
mirahazall <s223530601@deakin.edu.au>
2024-08-24 21:23:31 +1000

    docker build image shell script changes

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -15,4 +15,12 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-docker build -t $CONTAINER_REGISTRY/video-streaming:$VERSION --file ./Dockerfile-prod .
+# Enable Docker Buildx for multi-platform builds
+docker buildx create --use
+
+# Build and push the Docker image for both ARM and x86_64 platforms
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t "$CONTAINER_REGISTRY/video-streaming:$VERSION" \
+  --file ./Dockerfile-prod \
+  --push .
